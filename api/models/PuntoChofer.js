90c1d37e49a1d5c4d647126dd1b12ee9dedc6e29
Brayan <brayan.yevenes1501@alumnos.ubiobio.cl>
2020-05-21 20:11:51 -0400

    Cambios y comentarios de modelos agregados

--- api/models/PuntoChofer.js
+++ api/models/PuntoChofer.js
@@ -4,6 +4,14 @@ const moment = require('moment');
 
 let Schema = mongoose.Schema;
 
+/**
+ * Modelo del punto para la utilización del modelo del puntoChoferSchema.
+ *
+ * En este modelo se piden 2 cosas
+ * 1.- Un valor por default Point para señalar de que tipo de String es.
+ * 2.- la coordinates que sera un array de numero que sera necesario.
+ */
+
 let pointSchema = new Schema({
   type: {
     type: String,
@@ -14,6 +22,16 @@ let pointSchema = new Schema({
     required: true,
   },
 });
+
+/**
+ * Modelo puntoChoferSchema para la utilización de marcar el punto de dejada de basura
+ * en el punto del vertedero.
+ *
+ * En este modelo se piden 3 cosas
+ * 1.- El chofer, para identificar que persona deja la basura en el vertedero
+ * 2.- la fecha que no es necesaria ingresar dado que por default se creara al crear el punto
+ * 3.- location sera el punto de cordenadas de latitude y longitud de donde se deja la basura
+ */
 
 let puntoChoferSchema = new Schema({
   chofer: {
--- api/models/Recorrido.js
+++ api/models/Recorrido.js
@@ -6,6 +6,14 @@ moment.locale('es');
 
 let Schema = mongoose.Schema;
 
+/**
+ * Modelo del poligono de puntos para la utilización del modelo del recorridoSchema
+ *
+ * En este modelo se piden 2 cosas
+ * 1.- Un valor por default Polygon para señalar de que tipo de String es.
+ * 2.- la coordinates que sera un triple array de numeros para tener un conjunto de puntos.
+ */
+
 let polygonSchema = new Schema({
   type: {
     type: String,
@@ -16,6 +24,19 @@ let polygonSchema = new Schema({
     required: true,
   },
 });
+
+/**
+ * Modelo recorridoSchema para la utilización de marcacar un polygono de puntos con respecto al recorrido
+ *
+ * En este modelo se pieden 7 cosas
+ * 1.- La población que estara asociado el recorrido la cual es necesaria.
+ * 2.- El nombre que tendra el recorrido tambien es requerido.
+ * 3.- La descripción del recorrido de igual manera requerido.
+ * 4.- El estado del recorrido que por default sera true, si este se encuentra operativo o no.
+ * 5.- La localización de los puntos del polygono que sera requeridos.
+ * 6.- La fecha de inicio del recorrido que sera necesaria.
+ * 7.- La fecha de termino del recorrido que tambien sera obligatoria.
+ */
 
 let recoridoSchema = new Schema({
   poblacion: {
@@ -42,10 +63,12 @@ let recoridoSchema = new Schema({
   date_recorrido_iniciado: {
     type: String,
     default: () => moment().format('dddd, h:mm:ss a'),
+    required: [true, 'El horario de inicio es necesario'],
   },
   date_recorrido_finalizado: {
     type: String,
     default: () => moment().format('dddd, h:mm:ss a'),
+    required: [true, 'El horario de termino es necesario'],
   },
 });
 
