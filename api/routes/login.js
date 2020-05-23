95d91527cb8cd91a2d34e5592fbe245819821993
Brayan <brayan.yevenes1501@alumnos.ubiobio.cl>
2020-05-23 18:32:30 -0400

    colocamos routes nuevamente para poder subir a la nube

---
+++ api/routes/login.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const Usuario = require('../models/Usuario');
+const app = express();
+
+/**
+ * =====================================
+ * Loguearse Nuevo Usuario, se verifica
+ * tanto como email y contraseñas
+ * y si esta contraseña hace match
+ * con la de la base de datos de manera
+ * asyncrona.
+ * =====================================
+ */
+
+app.post('/', (req, res) => {
+  let body = req.body;
+
+  Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        err,
+      });
+    }
+
+    if (!usuarioDB) {
+      return res.status(400).json({
+        ok: false,
+        err: {
+          message: '(Usuario) o contraseña incorrectos',
+        },
+      });
+    }
+
+    if (!bcrypt.compareSync(body.password, usuarioDB.password)) {
+      return res.status(400).json({
+        ok: false,
+        err: {
+          message: 'Usuario o (contraseña) incorrectos',
+        },
+      });
+    }
+
+    let token = jwt.sign(
+      {
+        usuario: usuarioDB,
+      },
+      process.env.SEEDD,
+      { expiresIn: 60 * 60 * 24 * 365 }
+    );
+
+    res.json({
+      ok: true,
+      usuario: usuarioDB,
+      token,
+    });
+  });
+});
+
+module.exports = app;
